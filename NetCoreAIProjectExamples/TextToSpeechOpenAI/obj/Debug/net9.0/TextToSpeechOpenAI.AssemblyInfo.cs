0c182ddd784ba79469a6fa8eac837fb0b058d1a8
emreakturk11 <emreakturk25@gmail.com>
2025-03-27 12:03:34 +0300

    Sentiment analizing for text

--- NetCoreAIProjectExamples/TextToSpeechOpenAI/obj/Debug/net9.0/TextToSpeechOpenAI.AssemblyInfo.cs
+++ NetCoreAIProjectExamples/TextToSpeechOpenAI/obj/Debug/net9.0/TextToSpeechOpenAI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextToSpeechOpenAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6020c679e9c2abd5f9c71bc8533d9e6818126e61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e84cb58f2410765130f596ab15b4ef1939d00d25")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextToSpeechOpenAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextToSpeechOpenAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
